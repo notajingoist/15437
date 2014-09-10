c5f3c733fed9b0b96a94fc12d40580ce1e256c8f
Jing Xiao <notajingoist@gmail.com>
2014-09-10 14:27:36 -0400

    added individual url routes for the different actions for the course app from last recitation

--- in-class/2014-09-08 Introduction to Django/courseproj/courseapp/urls.py
+++ in-class/2014-09-08 Introduction to Django/courseproj/courseapp/urls.py
@@ -6,5 +6,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'courseproj.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'courseapp.views.index'),
+    url(r'^$', 'courseapp.views.home'),
+    url(r'^create-student$', 'courseapp.views.create_student'),
+    url(r'^create-course$', 'courseapp.views.create_course'),
+    url(r'^register-for-course$', 'courseapp.views.register_for_course'),
 )
--- in-class/2014-09-08 Introduction to Django/courseproj/courseapp/views.py
+++ in-class/2014-09-08 Introduction to Django/courseproj/courseapp/views.py
@@ -2,17 +2,33 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def home(request):
+    return render(request, 'index.html', {})
+
+
+def create_student(request):
     context = {}
     
     context['student_name'] = ''
     if 'student_name' in request.GET:
         context['student_name'] = request.GET['student_name']
+
+    return render(request, 'index.html', context)
+
+
+def create_course(request):
+    context = {}
     
     context['course_name'] = ''
     if 'course_name' in request.GET:
         context['course_name'] = request.GET['course_name']
 
+    return render(request, 'index.html', context)
+
+
+def register_for_course(request):
+    context = {}
+   
     context['registered_student_name'] = ''
     context['registered_course_name'] = ''
     if 'registered_student_name' in request.GET \
