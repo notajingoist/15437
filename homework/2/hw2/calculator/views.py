56df98a6fd3058e11b04ec98b21fd5df6687a15b
Jing Xiao <notajingoist@gmail.com>
2014-09-11 17:49:50 -0400

    fixed zero issue maybe

--- homework/2/hw2/calculator/views.py
+++ homework/2/hw2/calculator/views.py
@@ -6,6 +6,7 @@ def home(request):
 
 def calculate(request):
 	divide_by_zero_error = 'divide by zero error'
+	invalid_input_error = 'invalid input error'
 
 	context = {}
 
@@ -14,43 +15,50 @@ def calculate(request):
 	context['answer_op'] = ''
 	context['answer_arg2'] = ''
 	context['answer_equals'] = ''
-	
 
 	operators = ['/', '*', '-', '+', '=']
+	digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-	if 'display' in request.GET:
-		context['answer_display'] = request.GET['display']
+	if 'display' in request.POST:
+		context['answer_display'] = request.POST['display']
 
-	if 'arg1' in request.GET:
-		context['answer_arg1'] = request.GET['arg1']
+	if 'arg1' in request.POST:
+		context['answer_arg1'] = request.POST['arg1']
 
-	if 'op' in request.GET:
-		context['answer_op'] = request.GET['op']
+	if 'op' in request.POST:
+		context['answer_op'] = request.POST['op']
 
-	if 'arg2' in request.GET:
-		context['answer_arg2'] = request.GET['arg2']
+	if 'arg2' in request.POST:
+		context['answer_arg2'] = request.POST['arg2']
 
-	if 'input' in request.GET:
-		user_input = request.GET['input']
+	if 'input' in request.POST:
+		user_input = request.POST['input']
 
 		if user_input in operators:
 			# case: arg1, op, arg2 all exist -> evaluate
-			if 'arg1' in request.GET and request.GET['arg1'] \
-				and 'op' in request.GET and request.GET['op'] \
-				and 'arg2' in request.GET and request.GET['arg2']:
+			if 'arg1' in request.POST and request.POST['arg1'] \
+				and 'op' in request.POST and request.POST['op'] \
+				and 'arg2' in request.POST and request.POST['arg2']:
+				arg1 = str(int(request.POST['arg1']))
+				op = request.POST['op']
+				arg2 = str(int(request.POST['arg2']))
 
-				if request.GET['op'] == '/' and request.GET['arg2'] == '0':
+				if op == '/' and arg2 == '0':
 					context['answer_display'] = divide_by_zero_error #divide by zero
 					
 					# reset everything
 					context['answer_arg1'] = ''
 					context['answer_op'] = ''
 					context['answer_arg2'] = ''
+				elif op == '=':
+					context['answer_display'] = invalid_input_error #divide by zero
+					
+					# reset everything
+					context['answer_arg1'] = ''
+					context['answer_op'] = ''
+					context['answer_arg2'] = ''
 				else:
-					context['answer_display'] = eval(request.GET['arg1'] \
-									+ request.GET['op'] \
-									+ request.GET['arg2'])
-
+					context['answer_display'] = eval(arg1 + op + arg2)
 					context['answer_arg1'] = context['answer_display'] #set answer to be new arg1
 					context['answer_arg2'] = '' #reset arg2
 
@@ -58,38 +66,45 @@ def calculate(request):
 						context['answer_op'] = user_input #set new op
 					else:
 						context['answer_op'] = ''
-						# context['answer_equals'] = 'equals'
+						context['answer_equals'] = 'equals' #indicate op is =
 
 			# case: arg1, op exist -> replace op
-			elif 'arg1' in request.GET and request.GET['arg1'] \
-				and 'op' in request.GET and request.GET['op']:
+			elif 'arg1' in request.POST and request.POST['arg1'] \
+				and 'op' in request.POST and request.POST['op']:
+				arg1 = str(int(request.POST['arg1']))
+				op = request.POST['op']
+
 				if user_input != '=':
 					context['answer_display'] = context['answer_display'] # don't change display
 					context['answer_op'] = user_input #set new op
 				else:
-					if request.GET['op'] == '/' and request.GET['arg1'] == '0':
+					if op == '/' and arg1 == '0':
 						context['answer_display'] = divide_by_zero_error #divide by zero
 
 						# reset everything
 						context['answer_arg1'] = ''
 						context['answer_op'] = ''
 						context['answer_arg2'] = ''
+					elif op == '=':
+						context['answer_display'] = invalid_input_error #divide by zero
+						
+						# reset everything
+						context['answer_arg1'] = ''
+						context['answer_op'] = ''
+						context['answer_arg2'] = ''
 					else:
-						context['answer_display'] = eval(request.GET['arg1'] \
-									+ request.GET['op'] \
-									+ request.GET['arg1'])
-
-						context['answer_op'] = request.GET['op'] #keep old op
+						context['answer_display'] = eval(arg1 + op + arg1)
+						context['answer_op'] = op #keep old op
 
 			# case: arg1 exists -> save op
-			elif 'arg1' in request.GET and request.GET['arg1']:
+			elif 'arg1' in request.POST and request.POST['arg1']:
 				if user_input != '=':
 					context['answer_display'] = context['answer_display'] # don't change display
 					context['answer_op'] = user_input #set op
 				else:
 					context['answer_display'] = context['answer_display'] # don't change display
 					context['answer_op'] = ''
-					# context['answer_equals'] = 'equals'
+					context['answer_equals'] = 'equals' #indicate op is =
 
 			# case: no arg exists -> use 0 as arg1
 			else:
@@ -97,39 +112,53 @@ def calculate(request):
 					context['answer_arg1'] = '0'
 					context['answer_op'] = user_input
 
-		else: #not an operator
+		elif user_input in digits: #not an operator
 			# case: arg1, op, arg2 all exist -> continuing arg2
-			if 'arg1' in request.GET and request.GET['arg1'] \
-				and 'op' in request.GET and request.GET['op'] \
-				and 'arg2' in request.GET and request.GET['arg2']:
-				context['answer_display'] += user_input
-				context['answer_arg2'] += user_input
+			if 'arg1' in request.POST and request.POST['arg1'] \
+				and 'op' in request.POST and request.POST['op'] \
+				and 'arg2' in request.POST and request.POST['arg2']:
+				arg2 = str(int(request.POST['arg2']))
+
+				if arg2 == '0':
+					context['answer_display'] = user_input
+					context['answer_arg2'] = user_input
+				else:
+					context['answer_display'] += user_input
+					context['answer_arg2'] += user_input
 
 			# case: arg1, op exist -> starting arg2
-			elif 'arg1' in request.GET and request.GET['arg1'] \
-				and 'op' in request.GET and request.GET['op']:
+			elif 'arg1' in request.POST and request.POST['arg1'] \
+				and 'op' in request.POST and request.POST['op']:
 				context['answer_display'] = user_input
 				context['answer_arg2'] = user_input
-
-			# case: arg1 exists, op was equals -> starting arg2
-			elif 'arg1' in request.GET and request.GET['arg1'] \
-				and 'equals' in request.GET and request.GET['equals']:
-				context['answer_display'] = user_input
-				context['answer_arg2'] = user_input
-				# context['answer_equals'] = ''
 
 			# case: arg1 exists -> continuing arg1
-			elif 'arg1' in request.GET and request.GET['arg1']:
-				context['answer_display'] += user_input
-				context['answer_arg1'] += user_input
+			elif 'arg1' in request.POST and request.POST['arg1']:
+				if 'equals' in request.POST:
+					context['answer_display'] = user_input
+					context['answer_arg1'] = user_input
+				else:
+					arg1 = str(int(request.POST['arg1']))
+
+					if arg1 == '0':
+						context['answer_display'] = user_input
+						context['answer_arg1'] = user_input
+					else:
+						context['answer_display'] += user_input
+						context['answer_arg1'] += user_input
 
 			# case: no arg exists -> starting arg1
 			else:
 				context['answer_display'] = user_input
 				context['answer_arg1'] = user_input
 
+		else:
+			context['answer_display'] = invalid_input_error
+			
+			# reset everything
+			context['answer_arg1'] = ''
+			context['answer_op'] = ''
+			context['answer_arg2'] = ''
+
 
 	return render(request, 'index.html', context)
-
-	# if 'answer_state' in request.GET:
-	# 	request.GET
