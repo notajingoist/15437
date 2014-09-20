228692b5d22f02097415aae62a41c7cc04ea32ed
Jing Xiao <notajingoist@gmail.com>
2014-09-20 01:04:01 -0400

    unfinished in-class stuff

--- in-class/2014-09-10 Django data models/courseproj/courseapp/models.py
+++ in-class/2014-09-10 Django data models/courseproj/courseapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    andrew_id = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __unicode__(self):
+        return self.andrew_id
+
+class Course(models.Model):
+    course_number = models.CharField(max_length=10)
+    course_name = models.CharField(max_length=100)
+    students = models.ManyToManyField(Student)
+
+    def __unicode__(self):
+        return self.course_name
--- in-class/2014-09-10 Django data models/courseproj/courseapp/views.py
+++ in-class/2014-09-10 Django data models/courseproj/courseapp/views.py
@@ -3,15 +3,34 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html', {})
+    all_courses = Course.objects.all()
+    all_students = Student.objects.all()
+
+    context = {}
+    context['courses'] = all_courses;
+    contexte['students'] = all_students;
+
+    return render(request, 'index.html', context)
 
 
 def create_student(request):
+    errors = []
+    if not 'andrew_id' in request.GET or not request.GET['item']:
+        errors.append('You must enter an andrew ID to create a student.')
+    if not 'first_name' in request.GET or not request.GET['first_name'
+
+
     context = {}
     
-    context['student_name'] = ''
-    if 'student_name' in request.GET:
-        context['student_name'] = request.GET['student_name']
+    context['andrew_id'] = ''
+    context['first_name'] = ''
+    context['last_name'] = ''
+    if 'andrew_id' in request.GET \
+        and 'first_name' in request.GET \ 
+        and 'last_name' in request.GET:
+        context['andrew_id'] = request.GET['andrew_id']
+        context['first_name'] = request.GET['first_name']
+        context['last_name'] = request.GET['last_name']
 
     return render(request, 'index.html', context)
 
@@ -19,8 +38,11 @@ def create_student(request):
 def create_course(request):
     context = {}
     
+    context['course_number'] = ''
     context['course_name'] = ''
-    if 'course_name' in request.GET:
+    if 'course_number' in request.GET \
+        and 'course_name' in request.GET:
+        context['course_number'] = request.GET['course_number']
         context['course_name'] = request.GET['course_name']
 
     return render(request, 'index.html', context)
@@ -29,11 +51,11 @@ def create_course(request):
 def register_for_course(request):
     context = {}
    
-    context['registered_student_name'] = ''
-    context['registered_course_name'] = ''
-    if 'registered_student_name' in request.GET \
-        and 'registered_course_name' in request.GET:
-        context['registered_student_name'] = request.GET['registered_student_name']
-        context['registered_course_name'] = request.GET['registered_course_name']
+    context['andrew_id'] = ''
+    context['course_name'] = ''
+    if 'andrew_id' in request.GET \
+        and 'course_name' in request.GET:
+        context['andrew_id'] = request.GET['andrew_id']
+        context['course_name'] = request.GET['course_name']
 
     return render(request, 'index.html', context)
