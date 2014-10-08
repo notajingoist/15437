593663486787ce95503b145acc7bf1c1c6d6c7ef
Jing Xiao <notajingoist@gmail.com>
2014-10-08 17:25:44 -0400

    added track 2 for jquery in class exercise

--- in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
+++ in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
@@ -1,8 +1,9 @@
 // Insert code here to run when the DOM is ready
 $(document).ready( function() {
 
-	var $deleteBtns = $('.delete-btn');
-	$deleteBtns.on('click', deleteItem);
+	//var $deleteBtns = $('.delete-btn');
+	var $itemList = $('#todo-list');
+	$itemList.on('click', '.delete-btn', deleteItem);
 
 	function deleteItem(e) {
 		var id = $(this).attr('data-item-id');
@@ -14,9 +15,34 @@ $(document).ready( function() {
 			{ csrfmiddlewaretoken: csrf }
 		);
 
-		deleteRequest.done(function() {
+		deleteRequest.done(function(data) {
 			$item.parent().remove();
 		});
+	}
+
+	var $form = $('form');
+	$form.off();
+	$form.on('click', 'input[type="submit"]', addItem);
+
+	function addItem(e) {
+		var csrf = $('input[name="csrfmiddlewaretoken"]').val();
+		var inputText = $('input[name="item"]').val();
+		var addRequest = $.post(
+			'/shared-todo-list/add-item',
+			{ 
+				csrfmiddlewaretoken: csrf,
+				item: inputText
+			},
+			function(data) {
+				var lastItem = $(data)
+									.filter('#todo-list')
+									.children()
+									.last();
+				$itemList.append(lastItem);
+			}
+		);
+
+		return false;
 	}
 
 });
