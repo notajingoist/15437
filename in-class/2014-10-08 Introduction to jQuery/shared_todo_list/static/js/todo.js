b7ad5c16eab6d36ae29463a3ffe8100c525b1471
Jing Xiao <notajingoist@gmail.com>
2014-10-08 15:05:55 -0400

    track 1 for jquery in class exercise

--- in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
+++ in-class/2014-10-08 Introduction to jQuery/shared_todo_list/static/js/todo.js
@@ -1,4 +1,22 @@
 // Insert code here to run when the DOM is ready
 $(document).ready( function() {
 
+	var $deleteBtns = $('.delete-btn');
+	$deleteBtns.on('click', deleteItem);
+
+	function deleteItem(e) {
+		var id = $(this).attr('data-item-id');
+		var csrf = $('input[name="csrfmiddlewaretoken"]').val();
+		var $item = $(this);
+
+		var deleteRequest = $.post(
+			'/shared-todo-list/delete-item/' + id, 
+			{ csrfmiddlewaretoken: csrf }
+		);
+
+		deleteRequest.done(function() {
+			$item.parent().remove();
+		});
+	}
+
 });
